97aa026e92f86fca4ed3e4337ebddbf98a74328a
Divad020 <davidpfleger960@gmail.com>
2022-04-14 20:11:47 +0200

    t

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      there fore u need to kys.
+      <a target="_blank" href="https://medium.com/@edabot/how-to-kill-yourself-14de9f38ca43">How to kys?</a> +
+      <a target="_blank" href="https://www.youtube.com/watch?v=s9EdQ2tRal0">Tutorial</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
